1ef10f6c4c971fcfb31f502e8e2c412bc2106901
Mehedi Hasan Mubin <mehedi.cseulab201@gmail.com>
2025-05-18 17:01:19 +0600

    lifted the state up from square to board

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,22 +1,29 @@
+import { useState } from "react";
 import Square from "./Square";
 
 const Board = () => {
+   const [squares, setSquares] = useState(Array(9).fill(null));
+
+  const handleClick = () => {
+    setSquares([...squares, ])
+  }
+
    return (
       <>
-         <div>
-            <Square />
-            <Square />
-            <Square />
+         <div className="flex">
+            <Square value={squares[0]} onSquareClick={handleClick}/>
+            <Square value={squares[1]} onSquareClick={handleClick}/>
+            <Square value={squares[2]} onSquareClick={handleClick}/>
          </div>
-         <div>
-            <Square />
-            <Square />
-            <Square />
+         <div className="flex">
+            <Square value={squares[3]} onSquareClick={handleClick}/>
+            <Square value={squares[4]} onSquareClick={handleClick}/>
+            <Square value={squares[5]} onSquareClick={handleClick}/>
          </div>
-         <div>
-            <Square />
-            <Square />
-            <Square />
+         <div className="flex">
+            <Square value={squares[6]} onSquareClick={handleClick}/>
+            <Square value={squares[7]} onSquareClick={handleClick}/>
+            <Square value={squares[8]} onSquareClick={handleClick}/>
          </div>
       </>
    );
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,8 +1,11 @@
-const Square = () => {
+const Square = ({ value, onSquareClick }) => {
    return (
       <>
-         <button className="bg-white border border-gray-400 text-lg h-12 w-12 m-1 leading-9">
-            X
+         <button
+            className="bg-white border border-gray-400 text-lg h-12 w-12 m-1 leading-9"
+            onClick={onSquareClick}
+         >
+            {value}
          </button>
       </>
    );
